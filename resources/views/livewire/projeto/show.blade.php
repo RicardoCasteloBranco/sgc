56eb4c3d1bee4ed8a759aa818a3fdd44ff8f573a
RicardoCasteloBranco <ricardo.castelobranco2@gmail.com>
2026-03-08 21:27:00 -0300

    Esboço da página de detalhamento do projeto

--- resources/views/livewire/projeto/show.blade.php
+++ resources/views/livewire/projeto/show.blade.php
@@ -6,4 +6,29 @@
         <p><strong>Objetivo Geral:</strong> {{ $projeto->curso->objetivo_geral ?? 'Não Disponível' }}</p>
         <p class="max-w-full overflow-x-auto whitespace-pre-wrap break-all"><strong>Objetivos Específicos:</strong> {{ $projeto->curso->objetivos_especificos ?? 'Não Disponível' }}</p>
         <p><strong>Ano:</strong> {{ $projeto->ano ?? 'Não Disponível' }}</p>
+    </div>
+    <div class="mt-4">
+        <h3 class="text-lg font-semibold mb-2">Disciplinas Associadas</h3>
+        <x-table>
+            <x-slot name="theaders">
+                <x-table.heading>Disciplina</x-table.heading>
+                <x-table.heading>Carga Horária</x-table.heading>
+                <x-table.heading>Ações</x-table.heading>
+            </x-slot>
+            <x-slot name="tbody">
+            </x-slot>
+        </x-table>
+    </div>
+    <div class="mt-4">
+        <h3 class="text-lg font-semibold mb-2">Turmas Associados</h3>
+        <x-table>
+            <x-slot name="theaders">
+                <x-table.heading>Turma</x-table.heading>
+                <x-table.heading>Data de Início</x-table.heading>
+                <x-table.heading>Alunos</x-table.heading>
+                <x-table.heading>Ações</x-table.heading>
+            </x-slot>
+            <x-slot name="tbody">
+            </x-slot>
+        </x-table>
 </div>
